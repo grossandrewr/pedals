de00c6eaf33efc24df8cba99fd226a03e5c8e2e6
Andrew Gross <gross.andrewr@gmail.com>
2021-12-15 09:20:54 -0500

    Refactor styling structure

--- app/containers/RainPedal/index.js
+++ app/containers/RainPedal/index.js
@@ -1,139 +1,170 @@
 import 'regenerator-runtime/runtime';
-import React, {useState} from 'react';
-import { OuterBody, MainBody, ButtonContainer, StartStopButton, KnobContainer,
-        Box, Circle, Dot, KnobLabel } from './styled';
-import Grid from '@material-ui/core/Grid';
-
+import React, { useState } from 'react';
 import * as Tone from 'tone';
-import {wholeTone} from './constants'
 import {
-    generateSynths,
-    generatePanners,
-    generateRandNote,
-    generateRandPan,
-    randomizeTempo,
-    startTone,
-    stopTone }
-  from './utils'
+  OuterBody,
+  MainBody,
+  ButtonContainer,
+  StartStopButton,
+  KnobContainer,
+  Box,
+  Circle,
+  Dot,
+  KnobLabel,
+  BoxContainer,
+  RobotRainTitle,
+} from './styled';
 
-//project setup
+import { wholeTone } from './constants';
+import {
+  generateSynths,
+  generatePanners,
+  generateRandNote,
+  generateRandPan,
+  randomizeTempo,
+} from './utils';
+
+// project setup
 let globalTempo = 50;
 let globalOctave = 3;
-let globalPitch = 0
+let globalPitch = 0;
+let deviceOn = false;
+
 Tone.Transport.bpm.value = globalTempo;
-const reverb = new Tone.Reverb(2).toDestination()
+const reverb = new Tone.Reverb(2).toDestination();
 
 // run loop
-const synthList = generateSynths()
-const pannerList = generatePanners()
+const synthList = generateSynths();
+const pannerList = generatePanners();
 
 let counter = 0;
-Tone.Transport.scheduleRepeat(time => {
-  let index = counter % 50
-  const synthA = synthList[index]
-  const pannerA = pannerList[index]
+Tone.Transport.scheduleRepeat(
+  time => {
+    const index = counter % 50;
+    const synthA = synthList[index];
+    const pannerA = pannerList[index];
 
-  const randNote = generateRandNote(wholeTone, globalPitch, 3, globalOctave);
-  const randPan = generateRandPan();
-  pannerA.set({pan: randPan})
+    const randNote = generateRandNote(wholeTone, globalPitch, 3, globalOctave);
+    const randPan = generateRandPan();
+    pannerA.set({ pan: randPan });
 
-  randomizeTempo(globalTempo);
-  counter++;
+    randomizeTempo(globalTempo);
+    counter++;
 
-  synthA.triggerAttackRelease(randNote, '32n').connect(pannerA).connect(reverb).toDestination()
-}, "8n", 0);
+    synthA
+      .triggerAttackRelease(randNote, '32n')
+      .connect(pannerA)
+      .connect(reverb)
+      .toDestination();
+  },
+  '8n',
+  0,
+);
+
+const startTone = async () => {
+  if (!deviceOn) {
+    await Tone.start();
+    console.log('audio is ready');
+    Tone.Transport.start();
+  } else {
+    Tone.Transport.stop();
+  }
+  deviceOn = !deviceOn;
+};
 
 export default function RainPedal() {
   const [active, setActive] = useState(false);
 
-  const turnKnob = (e) => {
+  const turnKnob = e => {
     const box = e.currentTarget;
 
-    //mouse position
-    let mX = e.clientX;
-    let mY = e.clientY;
+    // mouse position
+    const mX = e.clientX;
+    const mY = e.clientY;
 
-    //element data
-    let boxData = box.getBoundingClientRect();
-    let boxWidth = boxData.width;
-    let boxHeight = boxData.height;
-    let left = boxData.left;
-    let top = boxData.top;
+    // element data
+    const boxData = box.getBoundingClientRect();
+    const boxWidth = boxData.width;
+    const boxHeight = boxData.height;
+    const { left } = boxData;
+    const { top } = boxData;
 
-    //rotation
+    // rotation
     let rotate = 0;
-    let radians = 180 / Math.PI;
-    let center = {
-      x: left + (boxWidth / 2),
-      y: top + (boxHeight / 2)
-    }
+    const radians = 180 / Math.PI;
+    const center = {
+      x: left + boxWidth / 2,
+      y: top + boxHeight / 2,
+    };
 
     // arc points
-    let x = mX - center.x;
-    let y = mY - center.y;
-    let angle = Math.floor(Math.atan2(y, x) * radians);
-    let startAngle = 180;
+    const x = mX - center.x;
+    const y = mY - center.y;
+    const angle = Math.floor(Math.atan2(y, x) * radians);
+    const startAngle = 180;
 
     if (active) {
       if (angle > -180 && angle < 0) {
         rotate = angle + startAngle;
         box.style.transform = `rotate(${rotate}deg)`;
 
-        if (box.id == "tempo-box") {
+        if (box.id === 'tempo-box') {
           globalTempo = 50 + rotate * 1.2;
-        } else if (box.id == "octave-box") {
+        } else if (box.id ==='octave-box') {
           globalOctave = 3 + Math.floor(rotate / 60);
-        } else if (box.id == "pitch-box") {
+        } else if (box.id === 'pitch-box') {
           globalPitch = rotate / 180;
         }
       }
     }
-  }
+  };
 
   return (
     <OuterBody onMouseUp={() => setActive(false)}>
       <MainBody>
-        <h1 id="title">Robot Rain</h1>
-        <Grid>
-          <KnobContainer
-            onMouseDown={() => setActive(true)}
-            style={{ left: '25%'}}
-          >
-            <Box id="tempo-box" onMouseMove={turnKnob}>
+        <KnobContainer>
+          <BoxContainer style={{ top: '30px', left: '40px' }}>
+            <Box
+              id="tempo-box"
+              onMouseDown={() => setActive(true)}
+              onMouseMove={turnKnob}
+            >
               <Circle>
-                <Dot id="dot-1"></Dot>
+                <Dot id="dot-1" />
               </Circle>
             </Box>
             <KnobLabel>Tempo</KnobLabel>
-          </KnobContainer>
-          <KnobContainer
-            onMouseDown={() => setActive(true)}
-            style={{ left: '45%'}}
-          >
-            <Box id="octave-box" onMouseMove={turnKnob}>
+          </BoxContainer>
+          <BoxContainer style={{ top: '130px', left: '110px' }}>
+            <Box
+              id="octave-box"
+              onMouseDown={() => setActive(true)}
+              onMouseMove={turnKnob}
+            >
               <Circle>
-                <Dot id="dot-2"></Dot>
+                <Dot id="dot-2" />
               </Circle>
             </Box>
             <KnobLabel>Pitch</KnobLabel>
-          </KnobContainer>
-          <KnobContainer
-            onMouseDown={() => setActive(true)}
-            style={{ left: '65%'}}
-          >
-            <Box id="pitch-box" onMouseMove={turnKnob}>
+          </BoxContainer>
+          <BoxContainer style={{ top: '30px', left: '180px' }}>
+            <Box
+              id="pitch-box"
+              onMouseDown={() => setActive(true)}
+              onMouseMove={turnKnob}
+            >
               <Circle>
-                <Dot id="dot-3"></Dot>
+                <Dot id="dot-3" />
               </Circle>
             </Box>
             <KnobLabel>Detune</KnobLabel>
-          </KnobContainer>
-          <ButtonContainer>
-            <StartStopButton onClick={startTone}>Start</StartStopButton>
-            <StartStopButton onClick={stopTone}>Stop</StartStopButton>
-          </ButtonContainer>
-        </Grid>
+          </BoxContainer>
+        </KnobContainer>
+        <ButtonContainer item>
+          <StartStopButton onClick={startTone}>I/O</StartStopButton>
+        </ButtonContainer>
+        <RobotRainTitle id="title">Robot Rain</RobotRainTitle>
       </MainBody>
     </OuterBody>
-  )
+  );
 }
--- app/containers/RainPedal/styled.js
+++ app/containers/RainPedal/styled.js
@@ -1,56 +1,45 @@
 import styled from 'styled-components';
 import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
 
 export const OuterBody = styled(Grid)`
-  height:500px;
+  height: 800px;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-family: courier,arial,helvetica;
-`
+  font-family: courier, arial, helvetica;
+`;
 
 export const MainBody = styled(Grid)`
   border: 2px solid grey;
   border-radius: 10px;
-  width: 600px;
-  height: 400px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+  width: 330px;
+  height: 560px;
+  position: relative;
   box-shadow: 7px 10px;
-`
-
-export const ButtonContainer = styled(Grid)`
-  width: 65%;
-  display: flex;
-  justify-content: space-around;
-`
-
-export const StartStopButton = styled(Button)`
-  width:80px;
-  height: 40px;
-  font-family: courier,arial,helvetica;
-`
+  background-color: #b1d3c6;
+`;
 
 export const KnobContainer = styled(Grid)`
-  position: absolute;
-  width: 200px;
+  width: 330px;
   height: 200px;
-  top: 40%;
-`
+  position: relative;
+`;
+
+export const BoxContainer = styled(Grid)`
+  width: 100px;
+  height: 120px;
+  position: absolute;
+`;
 
 export const Box = styled(Grid)`
   width: 100px;
   height: 100px;
-  background-color: transparent;
   position: absolute;
-  left: 25%;
-  top: 25%;
+  background-color: transparent;
   transform-origin: center center;
   z-index: 1;
   border-radius: 50%;
-`
+`;
 
 export const Circle = styled(Grid)`
   width: 80px;
@@ -61,8 +50,9 @@ export const Circle = styled(Grid)`
   top: 50%;
   transform: translate(-50%, -50%);
   border: 2px solid black;
-  box-shadow: 0 0 10px grey, inset 0 0 10px 5px grey;
-`
+  box-shadow: 0 0 1px grey, inset 0 0 10px 5px grey;
+  background-color: #fff5eb;
+`;
 
 export const Dot = styled(Grid)`
   width: 8px;
@@ -72,10 +62,39 @@ export const Dot = styled(Grid)`
   top: 40px;
   left: 8px;
   border-radius: 50%;
-`
+`;
 
 export const KnobLabel = styled.h4`
   position: absolute;
-  left: 40%;
-  top: 80%;
-`
+  left: 25px;
+  bottom: 0px;
+  font-size: 15px;
+  margin: 0;
+`;
+
+export const ButtonContainer = styled(Grid)`
+  position: absolute;
+  left: 60px;
+  bottom: 140px;
+  width: 65%;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const StartStopButton = styled(Grid)`
+  width: 80px;
+  height: 40px;
+  font-family: courier, arial, helvetica;
+  font-size: 10px;
+  border: 2px solid black;
+  background-color: #cccccc;
+  border-radius: 2px;
+`;
+
+export const RobotRainTitle = styled.h3`
+  position: absolute;
+  left: 60px;
+  bottom: 20px;
+  color: #fff5eb;
+  -webkit-text-stroke: 1px black;
+`;
